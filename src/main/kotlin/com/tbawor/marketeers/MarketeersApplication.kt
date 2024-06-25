2cc2347f2fc95d96f1de536c1c351fccc94fde65
Tomasz Bawor <bawortomasz@gmail.com>
2024-06-25 18:19:11 +0200

    Initial commit

---
+++ src/main/kotlin/com/tbawor/marketeers/MarketeersApplication.kt
@@ -0,0 +1,11 @@
+package com.tbawor.marketeers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketeersApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketeersApplication>(*args)
+}
---
+++ src/test/kotlin/com/tbawor/marketeers/MarketeersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tbawor.marketeers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketeersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
