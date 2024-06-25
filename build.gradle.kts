384ce09324b95dcbcef314bd643f025d5044fe78
Tomasz Bawor <bawortomasz@gmail.com>
2024-06-25 18:27:53 +0200

    Add kotlin arrow

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("io.arrow-kt:arrow-core:1.2.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/tbawor/marketeers/MarketeersApplication.kt
+++ src/main/kotlin/com/tbawor/marketeers/MarketeersApplication.kt
@@ -1,11 +1,21 @@
 package com.tbawor.marketeers
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MarketeersApplication
+class MarketeersApplication : CommandLineRunner {
+    val logger: Logger = LoggerFactory.getLogger(MarketeersApplication::class.java)
+
+    override fun run(vararg args: String?) {
+      logger.info("Hello from Marketeers application!")
+    }
+
+}
 
 fun main(args: Array<String>) {
-	runApplication<MarketeersApplication>(*args)
+    runApplication<MarketeersApplication>(*args)
 }
